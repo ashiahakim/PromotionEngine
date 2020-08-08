2996ce144c407d819b26446c728893d1af8af4d3
AHakim <AHakim@Thetaxbackgroup.com>
2020-08-08 17:01:27 +0530

    Added the lookp for unit Price and promotion offer

--- PromotionEngine/Service/Implementation/PromotionService.cs
+++ PromotionEngine/Service/Implementation/PromotionService.cs
@@ -2,11 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using PromotionEngine.Model;
 
 namespace PromotionEngine.Service.Implementation
 {
     public class PromotionService : IPromotionService
     {
+        private Dictionary<string, int> _itemUnitPriceLookup;
+        private Dictionary<string, CountPriceModel> _promotionOfferLookup;
+        public PromotionService()
+        {
+            CreateItemUnitPriceLookup();
+            CreatePromotionLookup();
+        }
+
+        private void CreateItemUnitPriceLookup()
+        {
+            _itemUnitPriceLookup = new Dictionary<string, int>
+            {
+                { "a", 50 },
+                { "b", 30 },
+                { "c", 20 },
+                { "d", 15 },
+                { "e", 10 }
+            };
+        }
+
+        private void CreatePromotionLookup()
+        {
+            _promotionOfferLookup = new Dictionary<string, CountPriceModel>
+            {
+                { "a", new CountPriceModel{ Count = 3, Price = 130 } },
+                { "b", new CountPriceModel{ Count = 2, Price = 45 } }
+            };
+        }
         public int ApplyPromotion(Dictionary<string, int> formRequest)
         {
             throw new NotImplementedException();
