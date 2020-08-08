a5d97635190f2800fe413e9d1a263aa2d7ed3915
AHakim <AHakim@Thetaxbackgroup.com>
2020-08-08 17:22:27 +0530

    Added some test scenario to test the C and D items

--- PromotionEngine.Test/EnsurePromotionService.cs
+++ PromotionEngine.Test/EnsurePromotionService.cs
@@ -95,5 +95,61 @@ namespace PromotionEngine.Test
                 result.ShouldBe(165);
             }
         }
+
+        public class CheckCartPriceWithCAndDItems : EnsurePromotionService
+        {
+            private Dictionary<string, int> CreateInputRequest(int inputC, int inputD)
+            {
+                var input = new Dictionary<string, int>
+                {
+                    { "a", 0 },
+                    { "b", 0 }
+                };
+                input.Add("c", inputC);
+                input.Add("d", inputD);
+
+                return input;
+            }
+
+            [Fact]
+            public void SingleCItem()
+            {
+                var input = CreateInputRequest(1, 0);
+                var result = CallApplyPromotionWithGivenData(input);
+                result.ShouldBe(20);
+            }
+
+            [Fact]
+            public void SingleDItem()
+            {
+                var input = CreateInputRequest(0, 1);
+                var result = CallApplyPromotionWithGivenData(input);
+                result.ShouldBe(15);
+            }
+
+            [Fact]
+            public void SingleCSingleDItem()
+            {
+                var input = CreateInputRequest(1, 1);
+                var result = CallApplyPromotionWithGivenData(input);
+                result.ShouldBe(30);
+            }
+
+            [Fact]
+            public void MoreCLessD()
+            {
+                var input = CreateInputRequest(3, 2);
+                var result = CallApplyPromotionWithGivenData(input);
+                result.ShouldBe(80);
+            }
+
+            [Fact]
+            public void LessCMoreD()
+            {
+                var input = CreateInputRequest(2, 3);
+                var result = CallApplyPromotionWithGivenData(input);
+                result.ShouldBe(75);
+            }
+        }
     }
 }
